8d7173d296b013ad29817c966c31c1ca743ab1ee
Viktor Poklad <3d.poklad@gmail.com>
2021-08-03 12:39:17 +0300

    добавляет шаблоны

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+import menues from './menu.json';
+import menuesTmplate from './templates/menues_template.hbs';
+
+const targetList = document.querySelector('.js-menu');
+const listMarkup = menuesTmplate(menues);
+targetList.insertAdjacentHTML('beforeend', listMarkup);
+
+
+
+
+
