aef5e01ddcc9fdb81ccb4d1b16f4434a08a1803f
Viktor Poklad <3d.poklad@gmail.com>
2021-08-03 13:20:34 +0300

    Добавляет сохранение темы

--- src/index.js
+++ src/index.js
@@ -2,11 +2,41 @@ import './sass/main.scss';
 import menues from './menu.json';
 import menuesTmplate from './templates/menues_template.hbs';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+document.body.classList.add(Theme.LIGHT)
+
+chekTheme();
+
+
+themeSwitchToggle.addEventListener('change', onToggleChange);
+
+
 const targetList = document.querySelector('.js-menu');
 const listMarkup = menuesTmplate(menues);
 targetList.insertAdjacentHTML('beforeend', listMarkup);
 
+function onToggleChange(evt) {
+    if (evt.target.checked) {
+        localStorage.setItem('Theme', 'Dark')
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK)
+    }
+    else {
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT)
+        localStorage.setItem('Theme', 'Light')
+    };
+}
 
 
-
-
+function chekTheme() {
+    let localTheme = localStorage.getItem('Theme');
+    if (localTheme === 'Dark') {
+        themeSwitchToggle.checked = true;
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK)
+    }
+}
